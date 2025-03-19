97c812dc2a855cb52780182fce314b749d0fcf7f
SH Watanabe Souza <silviahwsouza@gmail.com>
2025-03-19 00:20:51 -0300

    Adição dos posts para o modelo e postagem do admin

--- meu_blog/blog/models.py
+++ meu_blog/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    resumo = models.CharField(max_length=300)
+    conteudo = RichTextField()
+    imagem = models.ImageField(upload_to='posts/')
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
